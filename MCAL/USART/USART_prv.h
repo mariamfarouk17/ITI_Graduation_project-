d781557c4fa0dd074295f20333142eab6de99ece
jjanaali45-debug <jjanaali45@gmail.com>
2026-08-10 15:03:24 +0300

    Add USART private header file with register definitions

---
+++ MCAL/USART/USART_prv.h
@@ -0,0 +1,14 @@
+#ifndef MCAL_USART_USART_PRV_H_
+#define MCAL_USART_USART_PRV_H_
+
+#define UDR			*((volatile u8*)(0x2C))
+#define UCSRA		*((volatile u8*)(0x2B))
+#define UCSRB		*((volatile u8*)(0x2A))
+#define UCSRC		*((volatile u8*)(0x40))
+#define UBRRL		*((volatile u8*)(0x29))
+#define UBRRH		*((volatile u8*)(0x40))
+
+#define UDRE	5
+#define RXC		7
+
+#endif
