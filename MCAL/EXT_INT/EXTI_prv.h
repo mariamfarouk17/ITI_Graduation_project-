413a8b5fb1ba6effa36c9e74d593cc3b70234ce1
jjanaali45-debug <jjanaali45@gmail.com>
2026-08-10 14:56:10 +0300

    Add EXTI_prv.h header file for external interrupt

---
+++ MCAL/EXT_INT/EXTI_prv.h
@@ -0,0 +1,10 @@
+#ifndef MCAL_EXTI_EXTI_PRV_H_
+#define MCAL_EXTI_EXTI_PRV_H_
+
+#define MCUCR		*((volatile u8*)(0x55))
+#define MCUCSR		*((volatile u8*)(0x54))
+#define GICR		*((volatile u8*)(0x5B))
+#define GIFR		*((volatile u8*)(0x5A))
+
+
+#endif
