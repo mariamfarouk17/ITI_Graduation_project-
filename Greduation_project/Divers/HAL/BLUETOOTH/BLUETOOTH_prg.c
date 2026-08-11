208861574de4fcae62b923056c0a2241797cd30a
jjanaali45-debug <jjanaali45@gmail.com>
2026-08-11 10:33:45 +0300

    Add Bluetooth initialization and communication functions

---
+++ Greduation_project/Divers/HAL/BLUETOOTH/BLUETOOTH_prg.c
@@ -0,0 +1,19 @@
+#include "BLUETOOTH_int.h"
+
+void BLUETOOTH_Init(void)
+{
+	MUSART_vInit();
+}
+
+void BLUETOOTH_Transmit(u8 A_u8Byte)
+{
+	MUSART_vTransmit();
+}
+
+u8 BLUETOOTH_Receive(void)
+{
+	return USART_Receive();
+}
+
+
+
