cd14e7e8abdc1f85f24faab76174e984b070a208
jjanaali45-debug <jjanaali45@gmail.com>
2026-08-12 10:13:49 +0300

    Update IR sensor header file with new includes and types

--- Greduation_project/Divers/HAL/IR/IR_int.h
+++ Greduation_project/Divers/HAL/IR/IR_int.h
@@ -1,11 +1,13 @@
 #ifndef IR_SENSOR_H_
 #define IR_SENSOR_H_
 
-#include <avr/io.h>
+#include "STD_TYPES.h"
+#include "DIO_interface.h"
 
-#define IR_PIN PA0
+#define IR_PORT    DIO_PORTA
+#define IR_PIN     DIO_PIN0
 
 void IR_Init(void);
-u8 IR_IsBlackLine(void);
+uint8_t IR_IsBlackLine(void);
 
 #endif
