44d69fb4f13421ea787c4550a7ea60e72d2316e8
mariam_attia20 <mariamattia94.75@gmail.com>
2026-08-12 13:36:27 +0300

    2 speeds for wheels edit IR sensor

--- Greduation_project/Divers/HAL/L298/L298_prg.c
+++ Greduation_project/Divers/HAL/L298/L298_prg.c
@@ -23,14 +23,23 @@ void L298_vInit(void)
 
     MTIMERS_vInit();
     MTIMERS_vStartTimer(TIM_0);
-    MTIMERS_vStartTimer(TIM_1_A);
-    MTIMERS_vSetPreloadValue(TIM_1_A, 1222u);
+    /* Start Timer1 (used for motor PWM) */
+    MTIMERS_vStartTimer(TIM_1);
+    /* Initialize both compare channels A and B */
+    MTIMERS_vSetCompareMatch(TIM_1_A, 1222u);
+    MTIMERS_vSetCompareMatch(TIM_1_B, 1222u);
 }
 
 
 
 void L298_vMove(u8 Copy_u8Direction, u8 Copy_u8Speed)
 {
+    L298_vMoveDualSpeed(Copy_u8Direction, Copy_u8Speed, Copy_u8Speed);
+}
+
+void L298_vMoveDualSpeed(u8 Copy_u8Direction, u8 Copy_u8SpeedLeft, u8 Copy_u8SpeedRight)
+{
+    /* Set motor direction pins based on requested direction */
     switch (Copy_u8Direction)
     {
         case MOVE_FORWARD:
@@ -70,29 +79,41 @@ void L298_vMove(u8 Copy_u8Direction, u8 Copy_u8Speed)
             break;
     }
 
-    if (Copy_u8Speed > 100u)
-    {
-        Copy_u8Speed = 100u;
-    }
+    /* Clamp speeds */
+    if (Copy_u8SpeedLeft > 100u) Copy_u8SpeedLeft = 100u;
+    if (Copy_u8SpeedRight > 100u) Copy_u8SpeedRight = 100u;
 
+    /* Enable/disable motor outputs */
     if (Copy_u8Direction == MOVE_STOP)
     {
-        MDIO_vSetPinVal(DIO_PORTB, ENA, DIO_LOW);
+        MDIO_vSetPinVal(DIO_PORTA, ENA, DIO_LOW);
         MDIO_vSetPinVal(DIO_PORTB, ENB, DIO_LOW);
     }
     else
     {
-        MDIO_vSetPinVal(DIO_PORTB, ENA, DIO_HIGH);
+        MDIO_vSetPinVal(DIO_PORTA, ENA, DIO_HIGH);
         MDIO_vSetPinVal(DIO_PORTB, ENB, DIO_HIGH);
 
-        if (Copy_u8Speed < 100u)
+        /* Convert speed (0-100) to compare value */
+        u16 L_u16PreloadA = (u16)((100u - Copy_u8SpeedLeft) * 1222u / 100u);
+        u16 L_u16PreloadB = (u16)((100u - Copy_u8SpeedRight) * 1222u / 100u);
+
+        if (Copy_u8SpeedLeft < 100u)
         {
-            u16 L_u16Preload = (u16)((100u - Copy_u8Speed) * 1222u / 100u);
-            MTIMERS_vSetPreloadValue(TIM_1_A, L_u16Preload);
+            MTIMERS_vSetCompareMatch(TIM_1_A, L_u16PreloadA);
         }
         else
         {
-            MTIMERS_vSetPreloadValue(TIM_1_A, 0u);
+            MTIMERS_vSetCompareMatch(TIM_1_A, 0u);
+        }
+
+        if (Copy_u8SpeedRight < 100u)
+        {
+            MTIMERS_vSetCompareMatch(TIM_1_B, L_u16PreloadB);
+        }
+        else
+        {
+            MTIMERS_vSetCompareMatch(TIM_1_B, 0u);
         }
     }
 }
--- Greduation_project/Divers/MCAL/TIMER/TMR_prg.c
+++ Greduation_project/Divers/MCAL/TIMER/TMR_prg.c
@@ -263,6 +263,10 @@ void MTIMERS_vSetCompareMatch(u8 A_u8TimerID , u16 A_u16OCR_val)
 	case TIM_1_A:
 		OCR1A = A_u16OCR_val;
 		break;
+
+	case TIM_1_B:
+		OCR1B = A_u16OCR_val;
+		break;
 	}
 }
 
